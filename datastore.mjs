f876bb3c1836e029b6b0ee621efc55a21bf415d9
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-02-11 15:40:38 +0900

    cleanup

--- datastore.mjs
+++
@@ -1,9 +0,0 @@
-import { DataStore } from '@aws-amplify/datastore';
-import { Blog } from './models';
-
-await DataStore.save(
-    new Blog({
-		"name": "Lorem ipsum dolor sit amet",
-		"posts": []
-	})
-);
