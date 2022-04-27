1168553bc847234674817296458b586924c8bdb4
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-28 06:30:20 +0900

    add Mui theme

--- boyaki/src/App.tsx
+++ boyaki/src/App.tsx
@@ -1,10 +1,53 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { createTheme } from "@mui/material";
+import { orange } from "@mui/material/colors";
 import { Amplify } from "aws-amplify";
 import "./App.css";
 import awsExports from "./aws-exports";
-console.log(awsExports);
 Amplify.configure(awsExports);
+
+// https://mui.com/material-ui/customization/theming/
+// If you are using TypeScript, you would also need to use module augmentation
+declare module "@mui/material/styles" {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1EA1F2",
+      contrastText: "#fff",
+    },
+    background: {
+      default: "#15202B",
+      paper: "#15202B",
+    },
+    divider: "#37444C",
+  },
+  // overrides: {
+  //   MuiButton: {
+  //     color: "white",
+  //   },
+  // },
+  typography: {
+    fontFamily: ["Arial"].join(","),
+  },
+  status: {
+    danger: orange[500],
+  },
+});
 
 function App() {
   return (
